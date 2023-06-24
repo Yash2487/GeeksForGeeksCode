ac951461554bfd9fd170e4481fb0072c582a16d5
Yash <yash.mate4494@gmail.com>
2023-06-24 11:50:43 +0530

    Right Triangle Pattern

--- src/com/programs/pattern/RightTrianglePattern.java
+++ src/com/programs/pattern/RightTrianglePattern.java
@@ -13,7 +13,7 @@ public class RightTrianglePattern {
 
 	public static void main(String[] args) {
 		int n = 5;
-		for (int i = 1; i <= 5; i++) {
+		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= i; j++) {
 				System.out.print("* ");
 			}
